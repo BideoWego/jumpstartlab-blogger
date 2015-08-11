79c34dcc166728e2b9169eab10d54c199d20a46c
Chris Scavello <bideowego@gmail.com>
2015-08-11 17:31:18 -0400

    Added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'articles#index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   match ':controller(/:action(/:id))', :via => [:get, :post]
 
